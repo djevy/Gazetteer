f12942b4130766841d6a91115c7d5d4d17279709
djevy <60827567+djevy@users.noreply.github.com>
2021-01-27 16:38:57 +0000

    Added tiles
    Added tiles to the map

--- js/script.js
+++ js/script.js
@@ -8,5 +8,17 @@ $(window).on('load', function () {
 });
 
 //Creating a map:
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+const mymap = L.map('map').setView([51.505, -0.09], 13);
 
+const attribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}';
+
+const tiles = L.tileLayer(tileUrl, { 
+    attribution,
+    subdomains: 'abcd',
+	minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+ });
+tiles.addTo(mymap);
